cdde1ef790a5520a16af8b1b46dd5af33be590f2
Atishay <atishaysoni1801@gmail.com>
2024-06-01 23:23:04 +0530

    Modified server.js and added vercel.json

--- server/server.js
+++ server/server.js
@@ -7,7 +7,11 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+	origin: [],
+	methods: ["POST", "GET"],
+	credentials: true
+}));
 
 app.use(bodyParser.json());
 
